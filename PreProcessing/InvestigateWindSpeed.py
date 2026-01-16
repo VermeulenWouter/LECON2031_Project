703c56d52a2e8d0a9b507db01b2a32b508211703
Wouter <woutervermeulen04@protonmail.com>
2026-01-16 22:05:35 +0100

    Add seasonality check and treatment + some optimisations for plotting

--- PreProcessing/InvestigateWindSpeed.py
+++ PreProcessing/InvestigateWindSpeed.py
@@ -13,19 +13,9 @@ import os.path as path
 import pandas as pd
 from scipy.stats import t
 
-from Helper import load_timeseries_data
+from Helper import correlation_matrix, load_timeseries_data
 from Helper.Plots import plot_series, plot_series_with_zooms
-
-
-def correlation_matrix(df, stations):
-    C = pd.DataFrame(index=stations, columns=stations, dtype=float)
-    for i, station_i in enumerate(stations):
-        for j, station_j in enumerate(stations):
-            if i <= j:
-                corr_ij = df[station_i].corr(df[station_j])
-                C.at[station_i, station_j] = corr_ij
-                C.at[station_j, station_i] = corr_ij
-    return C
+from Helper.TestSeasonality import test_seasonality, remove_given_seasonalities
 
 
 def station_deviation_stats(df):
@@ -103,60 +93,78 @@ def plot_windrose_with_speed(df, filepath: str | None = None, area_proportional:
     :param area_proportional: Whether to use area-proportional representation.
     :param bins: Number of direction bins.
     """
-    edgecolor = "white"
 
     directions_deg = pd.to_numeric(df["AverageWindDirection"], errors="coerce")
-    valid_dir = directions_deg.dropna()
-    np.deg2rad(valid_dir)
-
     speeds = pd.to_numeric(df["AverageWindSpeed"], errors="coerce")
     sub = pd.DataFrame({"dir": directions_deg, "spd": speeds}).dropna()
     dirs = np.deg2rad(sub["dir"])
     spd = sub["spd"]
 
-    # Adaptive speed bins
     speed_bins = _make_speed_bins(spd)
     speed_labels = [f"{speed_bins[i]:.1f}–{speed_bins[i+1]:.1f}" for i in range(len(speed_bins)-1)]
     cmap = plt.get_cmap("viridis")
     speed_colors = [cmap(i / (len(speed_bins)-2)) for i in range(len(speed_bins)-1)]
 
     H, dir_edges, spd_edges = np.histogram2d(dirs, spd, bins=[bins, speed_bins])
+    P = H / H.sum() # sums to 1
+    P_dir = P.sum(axis=1)
+    n_dir = P.shape[0]
+    n_spd = P.shape[1] #
+    radii_bottom = np.zeros_like(P)
+    radii_top = np.zeros_like(P)
 
     if area_proportional:
-        R = np.sqrt(H)
+        for d in range(n_dir):
+            r2 = 0.0
+            for i in range(n_spd):
+                p = P[d, i]
+                radii_bottom[d, i] = np.sqrt(r2)
+                r2 += p
+                radii_top[d, i] = np.sqrt(r2)
+        max_r = radii_top.max()
     else:
-        R = H
-
-    widths = np.diff(dir_edges)
-    gap = widths * 0.05
-    widths = widths - gap
+        for d in range(n_dir):
+            r = 0.0
+            for i in range(n_spd):
+                p = P[d, i]
+                radii_bottom[d, i] = r
+                r += p
+                radii_top[d, i] = r
+        max_r = radii_top.max()
 
     plt.close("all")
     fig = plt.figure(figsize=(8, 8))
     ax = plt.subplot(111, projection="polar")
 
-    # Stacked bars
-    bottom = np.zeros(bins)
-    for i in range(len(speed_bins) - 1):
-        radii = R[:, i]
-        ax.bar(dir_edges[:-1]+widths/2, radii, width=widths, bottom=bottom, align="center", color=speed_colors[i], edgecolor=edgecolor, linewidth=0.8, label=speed_labels[i])
-        bottom += radii
+    widths = np.diff(dir_edges)
+    gap = widths * 0.05
+    widths = widths - gap
 
-    # Radial labels (percentages)
-    total = H.sum()
+    theta = dir_edges[:-1] + widths / 2
+    for i in range(n_spd):
+        ax.bar( theta, radii_top[:, i] - radii_bottom[:, i], width=widths, bottom=radii_bottom[:, i], align="center", color=speed_colors[i], edgecolor="white", linewidth=0.8, label=speed_labels[i] if i == 0 else None)
+    tick_r = np.linspace(0, max_r, 5)
     if area_proportional:
-        max_r = bottom.max()
-        tick_r = np.linspace(0, max_r, 5)
-        tick_percent = (tick_r**2) / total * 100
-        ax.set_rticks(tick_r)
-        ax.set_yticklabels([f"{p:.1f}%" for p in tick_percent])
+        tick_percent = (tick_r**2) * 100
+    else:
+        tick_percent = tick_r * 100
 
-    # Cardinal directions
+    ax.set_rticks(tick_r)
+    ax.set_yticklabels([f"{p:.1f}%" for p in tick_percent])
     ax.set_theta_zero_location("N")
     ax.set_theta_direction(-1)
-    ax.set_thetagrids(np.arange(0, 360, 45), labels=["N","NE","E","SE","S","SW","W","NW"])
+    ax.set_thetagrids(
+        np.arange(0, 360, 45),
+        labels=["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    )
 
-    ax.set_title("Wind Rose with Speed Distribution")
+    title = "Wind Direction Distribution with Speed Coloring"
+    if area_proportional:
+        title += " (Area-Proportional)"
+    else:
+        title += " (Radius-Proportional)"
+    ax.set_title(title)
+
     ax.legend(loc="lower left", bbox_to_anchor=(1.05, 0.5))
 
     if filepath:
@@ -260,6 +268,8 @@ if __name__ == "__main__":
 
     AVG_WINDDIRECTIONS_DATA_PATH = path.join(_FILE_PATH, "Data", "Weather", "WindDirectionsAvg.csv")
 
+    EXCLUDE_BORSSELE_ALPHA = True  # After analysis, this station does not cover the full time range
+
     # %%
     windspeed_df = load_timeseries_data(WINDSPEEDS_DATA_PATH)
     windspeed_df = windspeed_df.drop(columns=["Europoint"])  # Afer analysis, geographically redundant with Borssele Alpha and further away
@@ -269,6 +279,12 @@ if __name__ == "__main__":
     print(f"Loaded wind speed data from {WINDSPEEDS_DATA_PATH}")
     print(f"\tStations: {stations}")
     print(f"\tData sample:\n\t{windspeed_df.head()}")
+
+    if EXCLUDE_BORSSELE_ALPHA:
+        # After investigation, Borssele Alpha does not give data for the full time range (2015-2024), so to be excluded from the average
+        windspeed_df = windspeed_df.drop(columns=["Borssele Alpha"])
+        stations = windspeed_df.columns
+        print(f"\nExcluding Borssele Alpha from average wind speed calculation. Remaining stations: {stations}")
 
     #%%
     plot_series(windspeed_df, stations, path.join(VISUALISATION_PATH, "WindSpeeds_OverTime.png"), quantity="Wind Speed", unit="m/s")
@@ -305,6 +321,7 @@ if __name__ == "__main__":
 
     #%%
     winddirections_df = load_timeseries_data(AVG_WINDDIRECTIONS_DATA_PATH)
+    winddirections_df.set_index("DateTime (UTC)", inplace=True)
     windspeed_df = windspeed_df.assign(AverageWindDirection=winddirections_df["AverageWindDirection"])
 
     plot_windrose_with_speed(windspeed_df, filepath=path.join(VISUALISATION_PATH, "WindSpeeds_WindRose_AreaProportional.png"))
@@ -313,3 +330,45 @@ if __name__ == "__main__":
     #%%
     plot_daily_profile(windspeed_df, "AverageWindSpeed", include_ci=True, filepath=path.join(VISUALISATION_PATH, "WindSpeeds_DailyProfile.png"), ylabel="Wind Speed [m/s]", title="Typical Daily Wind Speed Profile")
     plot_yearly_profile(windspeed_df, "AverageWindSpeed", include_raw=True, include_ci=True, filepath=path.join(VISUALISATION_PATH, "WindSpeeds_YearlyProfile.png"), ylabel="Wind Speed [m/s]", title="Typical Yearly Wind Speed Profile")
+
+    # %%
+    # Check seasonality (same as for production data in InvestigateProductionData.py)
+    test_seasonality(windspeed_df, "AverageWindSpeed", freq="daily")
+    test_seasonality(windspeed_df, "AverageWindSpeed", freq="yearly")
+
+    df_tmp = windspeed_df.copy()
+    pattern_daily, f_test_daily, model_daily = test_seasonality(windspeed_df, "AverageWindSpeed", freq="daily")
+    df_tmp["AverageWindSpeed_daily_removed"] = windspeed_df["AverageWindSpeed"] - windspeed_df.index.hour.map(pattern_daily)
+    pattern_yearly, f_test_yearly, model_yearly = test_seasonality(df_tmp, "AverageWindSpeed_daily_removed", freq="yearly")
+
+    # Daily
+    md = model_daily
+    fd = f_test_daily
+    r2_d = md.rsquared
+    f_d = float(fd.fvalue)
+    p_d = float(fd.pvalue)
+    c_d = "seasonality" if p_d < 0.05 else "no seasonality"
+
+    # Yearly
+    my = model_yearly
+    fy = f_test_yearly
+    r2_y = my.rsquared
+    f_y = float(fy.fvalue)
+    p_y = float(fy.pvalue)
+    c_y = "seasonality" if p_y < 0.05 else "no seasonality"
+
+    rows = ["AverageWindSpeed", r2_d, f_d, p_d, c_d, r2_y, f_y, p_y, c_y]
+
+    summary = pd.DataFrame([rows], columns=["Wind Farm", "Daily R²", "Daily F", "Daily p", "Daily Conclusion", "Yearly R²", "Yearly F", "Yearly p", "Yearly Conclusion"])
+    print(f"\nSeasonality Test Summary for Wind Speeds:")
+    print(summary)
+
+    df_windspeed_sadjusted = remove_given_seasonalities(windspeed_df, ["AverageWindSpeed"], {"AverageWindSpeed": pattern_daily}, {"AverageWindSpeed": pattern_yearly})
+
+    plot_daily_profile(df_windspeed_sadjusted, "AverageWindSpeed_sadjusted", include_ci=True, filepath=path.join(VISUALISATION_PATH, "WindSpeeds_DailyProfile_SeasonalityRemoved.png"), ylabel="Wind Speed [m/s]", title="Typical Daily Wind Speed Profile (Seasonality Removed)")
+    plot_yearly_profile(df_windspeed_sadjusted, "AverageWindSpeed_sadjusted", include_raw=True, include_ci=True, filepath=path.join(VISUALISATION_PATH, "WindSpeeds_YearlyProfile_SeasonalityRemoved.png"), ylabel="Wind Speed [m/s]", title="Typical Yearly Wind Speed Profile (Seasonality Removed)", ylim=(0, 12))
+
+    #%%
+    # Save
+    df_windspeed_sadjusted[["AverageWindSpeed", "AverageWindSpeed_sadjusted", "AverageWindSpeed_daily_season", "AverageWindSpeed_yearly_season"]].to_csv(path.join(_FILE_PATH, "Data", "Weather", "WindSpeedsAvg_SeasonalityRemoved.csv"), index=True, sep=";")
+
